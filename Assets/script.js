2b08132cb1b020540b716fa986dc6cb80593fe50
Chris Gibson <chrischarlesgibson@gmail.com>
2022-10-08 17:01:40 -0500

    add for loop to make 5 day forecast

--- Assets/script.js
+++ Assets/script.js
@@ -26,9 +26,9 @@ var date = moment().format("Do-MMM-GGGG");
 
 function getWeatherApi(cityName) {
   var cityCoordinates =
-    "https://api.openweathermap.org/geo/1.0/direct?q=%22" +
+    "https://api.openweathermap.org/geo/1.0/direct?q=" +
     cityInput +
-    "%22&appid=c5ad45b95de3366ffdd43c823c1307a9";
+    "&appid=c5ad45b95de3366ffdd43c823c1307a9";
   fetch(cityCoordinates)
     .then(function (response) {
       return response.json();
@@ -41,41 +41,70 @@ function getWeatherApi(cityName) {
         latitude +
         "&lon=" +
         longitude +
-        "&appid=c5ad45b95de3366ffdd43c823c1307a9&units=imperial";
+        "&exclude=minutely,hourly&include=daily&appid=c5ad45b95de3366ffdd43c823c1307a9&units=imperial";
       fetch(latLongCity)
         .then(function (response) {
           return response.json();
         })
         .then(function (data) {
           console.log(data);
-          displaySingleDay(data);
+          displayforecast(data);
         });
     });
 }
 
-var displaySingleDay = function (data) {
-  var singleCityName = document.createElement("h3");
-  var singleCityIcon = document.createElement("img");
-  var singleCityTemp = document.createElement("h3");
-  var singleCityWind = document.createElement("h3");
-  var singleCityHumd = document.createElement("h3");
+var displayforecast = function (data) {
+  for (i = 0; i < 6; i++) {
+    if (i === 0) {
+      var singleCityName = document.createElement("h3");
+      var singleCityIcon = document.createElement("img");
+      var singleCityTemp = document.createElement("h3");
+      var singleCityWind = document.createElement("h3");
+      var singleCityHumd = document.createElement("h3");
 
-  var iconCode = data.list[0].weather[0].icon;
-  var iconCodeUrl = "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
-  singleCityName.textContent = data.city.name + "  " + date;
-  singleCityIcon.setAttribute("src", iconCodeUrl);
-  singleCityTemp.textContent =
-    "Temp:" + " " + data.list[0].main.temp + " " + "F";
-  singleCityWind.textContent =
-    "Wind:" + " " + data.list[0].wind.speed + " " + "MPH";
-  singleCityHumd.textContent =
-    "Humidity:" + " " + data.list[0].main.humidity + " " + "%";
+      var iconCode = data.list[i].weather[0].icon;
+      var iconCodeUrl =
+        "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+      singleCityName.textContent = data.city.name + "  " + date;
+      singleCityIcon.setAttribute("src", iconCodeUrl);
+      singleCityTemp.textContent =
+        "Temp:" + " " + data.list[i].main.temp + " " + "F";
+      singleCityWind.textContent =
+        "Wind:" + " " + data.list[i].wind.speed + " " + "MPH";
+      singleCityHumd.textContent =
+        "Humidity:" + " " + data.list[i].main.humidity + " " + "%";
 
-  singleCard.append(singleCityName);
-  singleCard.append(singleCityIcon);
-  singleCard.append(singleCityTemp);
-  singleCard.append(singleCityWind);
-  singleCard.append(singleCityHumd);
+      singleCard.append(singleCityName);
+      singleCard.append(singleCityIcon);
+      singleCard.append(singleCityTemp);
+      singleCard.append(singleCityWind);
+      singleCard.append(singleCityHumd);
+    } else {
+      x = i * 8 - 1;
+      var newFiveDayCard = document.createElement("div");
+      var fiveCityIcon = document.createElement("img");
+      var fiveDayCityTemp = document.createElement("h3");
+      var fiveDayCityWind = document.createElement("h3");
+      var fiveDayCityHumd = document.createElement("h3");
+
+      var iconCode = data.list[x].weather[0].icon;
+      var iconCodeUrl =
+        "http://openweathermap.org/img/wn/" + iconCode + "@2x.png";
+      fiveCityIcon.setAttribute("src", iconCodeUrl);
+      fiveDayCityTemp.textContent =
+        "Temp:" + " " + data.list[x].main.temp + " " + "F";
+      fiveDayCityWind.textContent =
+        "Wind:" + " " + data.list[x].wind.speed + " " + "MPH";
+      fiveDayCityHumd.textContent =
+        "Humidity:" + " " + data.list[x].main.humidity + " " + "%";
+
+      newFiveDayCard.append(fiveCityIcon);
+      newFiveDayCard.append(fiveDayCityTemp);
+      newFiveDayCard.append(singleCityTemp);
+      newFiveDayCard.append(fiveDayCityWind);
+      newFiveDayCard.append(fiveDayCityHumd);
+    }
+  }
 };
 //   for (var i = 0; i < data.length; i++)
 //     var username = document.createElement("h3");
@@ -91,5 +120,5 @@ searchButton.addEventListener("click", function (event) {
 
   console.log("button pressed");
   getWeatherApi(cityInput);
-  displaySingleDay();
+  displayforecast();
 });
